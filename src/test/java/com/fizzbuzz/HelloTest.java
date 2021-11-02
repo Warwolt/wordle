9a5cd510d8955701af2ec97d46f232c710f1e99f
Rasmus <rasmus.kallqvist@gmail.com>
2021-11-02 21:52:55 +0100

    Add basic skeleton

---
+++ src/test/java/com/fizzbuzz/HelloTest.java
@@ -0,0 +1,11 @@
+package com.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloTest {
+    @Test
+    void helloTest() {
+            assertEquals(1 + 1, 2);
+    }
+}
