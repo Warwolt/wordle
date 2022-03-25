66d10cd5db49a21e9719323215d40848d6c26468
Rasmus <rasmus.kallqvist@gmail.com>
2022-03-25 08:37:29 +0100

    WIP

--- src/main/java/com/fizzbuzz/Main.java
+++ src/main/java/com/fizzbuzz/Main.java
@@ -1,7 +1,19 @@
 package com.fizzbuzz;
 
 public class Main {
+    class EscapeCode {
+        static final String RESET = "\033[0m";
+        static final String COLOR_BLACK = "\033[0;30m";
+        static final String COLOR_RED = "\033[0;31m";
+        static final String COLOR_GREEN = "\033[0;32m";
+        static final String COLOR_YELLOW = "\033[0;33m";
+        static final String COLOR_BLUE = "\033[0;34m";
+        static final String COLOR_PURPLE = "\033[0;35m";
+        static final String COLOR_CYAN = "\033[0;36m";
+        static final String COLOR_WHITE = "\033[0;37m";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hej världen!");
     }
 }
